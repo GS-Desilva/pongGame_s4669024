976b6e8552babda91b8b369357f09770a1410e5d
GS-Desilva <86835581+GS-Desilva@users.noreply.github.com>
2023-07-22 00:30:48 +0530

    updating method update to bounce ball off paddles and use of method on_touch_move

--- main.py
+++ main.py
@@ -37,20 +37,34 @@ class PongGame(Widget):
     #serve_ball method sets the initial position of the ball to the center
     #the default value of the optional argument vel is given as 4.0
     #the balls velocity is set to the value of the vel argument
-    def serve_ball(self, vel=(4.0)):
+    def serve_ball(self, vel=(4,0)):
         self.ball.center=self.center
         self.ball.velocity= vel
 
     def update(self, dt):
         self.ball.move()
 
-        #bounce off top and bottom
-        if(self.ball.y<0) or (self.ball.top> self.height):
+        #bounce off paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+        #bounce ball off bottom or top
+        if(self.ball.y<self.y) or (self.ball.top> self.top):
             self.ball.velocity_y*=-1
 
-        #bounce off left and right
-        if(self.ball.x<0) or (self.ball.right> self.width):
-            self.ball.velocity_x*=-1
+        #
+        if self.ball.x<self.x:
+            self.player2.score+=1
+            self.serve_ball(vel=(4,0))
+        if self.ball.right>self.width:
+            self.player1.score+=1
+            self.serve_ball(vel=(-4,0))
+
+    def on_touch_move(self, touch):
+        if touch.x<self.width/3:
+            self.player1.center_y=touch.y
+        if touch.x>self.width-self.width/3:
+            self.player2.center_y=touch.y
 
 class PongApp(App):
     def build(self):
